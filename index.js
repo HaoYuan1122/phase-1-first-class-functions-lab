b540bc04a1c22718af46f10606f677325652e1cc
Hao Yuan <haomichaelyuan@gmail.com>
2022-12-15 12:12:24 +1100

    commit tree 2cd68b28bf6c63b27848f1630abf1f18e978bfbe

--- index.js
+++ index.js
@@ -1,28 +1,70 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = a=> a.slice(0,2) 
+// const returnFirstTwoDrivers = a=> a.slice(0,2) 
 
-const returnLastTwoDrivers = b=> b.slice(-2)
+// const returnLastTwoDrivers = b=> b.slice(-2)
 
-const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+// const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
 
-function createFareMultiplier(integer) {
-    return function (fa){
-        return fa*integer
+// function createFareMultiplier(integer) {
+//     return function (fa){
+//         return fa*integer
+//     }
+// }
+
+// function fareDoubler(fare){
+//     return createFareMultiplier(2)(fare)
+// }
+
+// function fareTripler(fare){
+//     return createFareMultiplier(3)(fare)
+// }
+
+// function selectDifferentDrivers(c,d){
+//     if (d===returnFirstTwoDrivers){
+//         return returnFirstTwoDrivers(c)
+//     }else if (d===returnLastTwoDrivers){
+//         return returnLastTwoDrivers(c)
+//     }
+// } 
+
+const array1=['Antonia', 'Nuru', 'Amari', 'Mo']
+
+const returnFirstTwoDrivers = function (a){
+    return a.slice(0,2)
+}
+console.log(returnFirstTwoDrivers(array1))
+
+const returnLastTwoDrivers=function (a){
+    return a.slice(-2)
+}
+console.log(returnLastTwoDrivers(array1))
+
+const selectingDrivers=[returnFirstTwoDrivers,returnLastTwoDrivers]
+
+console.log(selectingDrivers[returnFirstTwoDrivers,returnLastTwoDrivers])
+
+function createFareMultiplier(integer){
+    return function (fare){
+        return integer*fare
     }
 }
 
-function fareDoubler(fare){
+const fareDoubler = function (fare){
     return createFareMultiplier(2)(fare)
 }
 
-function fareTripler(fare){
+const fareTripler = function (fare){
     return createFareMultiplier(3)(fare)
 }
+const fareTripler1 = function (fare){
+    return fare*3
+}
+console.log(fareTripler(9),fareTripler1(9))
 
-function selectDifferentDrivers(c,d){
-    if (d===returnFirstTwoDrivers){
-        return returnFirstTwoDrivers(c)
-    }else if (d===returnLastTwoDrivers){
-        return returnLastTwoDrivers(c)
+const selectDifferentDrivers = function (a,b){
+    if(b===returnFirstTwoDrivers){
+        return returnFirstTwoDrivers(a)
+    }else {
+        return returnLastTwoDrivers(a)
     }
-} 
+}
